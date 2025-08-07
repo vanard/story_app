36ddcac0874b119bbb9151e0a8ada2d831f87f88
vanard <vianixa34@gmail.com>
2025-08-07 19:56:54 +0700

    create static constants

---
+++ lib/static_constant.dart
@@ -0,0 +1,5 @@
+class StaticConstant {
+  static const messageToUser = 'Something went wrong. Please try again later.';
+  static const suffixMessageToUser = '\nPlease try again later.';
+  static const emptyListMessage = 'Page is not found.';
+}
