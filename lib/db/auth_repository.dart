45c27eb7dfa8d602409977cdcee01d2ba0999b22
vanard <vianixa34@gmail.com>
2025-08-13 10:27:50 +0700

    setup auth repo

--- lib/db/auth_repository.dart
+++ lib/db/auth_repository.dart
@@ -1,7 +1,66 @@
+import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:story_app/data/api_service.dart';
+import 'package:story_app/models/login_result.dart';
+import 'package:story_app/models/response.dart';
 
 class AuthRepository {
+  final String authKey = 'auth';
+  final String loginUserKey = 'loginUser';
+
   late final ApiService _apiService;
 
   AuthRepository({required ApiService service});
+
+  Future<bool> isLoggedIn() async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.getBool(authKey) ?? false;
+  }
+
+  Future<bool> login(String email, String pass) async {
+    final result = await _apiService.loginUser(email, pass);
+    debugPrint('Login result: $result');
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.setBool(authKey, true);
+  }
+
+  Future<bool> logout() async {
+    debugPrint('Logout');
+    final preferences = await SharedPreferences.getInstance();
+    
+    final loginUser = await getLoginUser();
+    if (loginUser != null) {
+      await deleteLoginUser(loginUser);
+    }
+
+    return preferences.setBool(authKey, false);
+  }
+
+  Future<void> register(String name, String email, String pass) async {
+    final result = await _apiService.registerUser(name, email, pass);
+    final resData = BaseResponse.fromJson(result);
+    debugPrint('Login result: $result');
+    debugPrint('Login result: ${resData.message}');
+    // final preferences = await SharedPreferences.getInstance();
+    // return preferences.setBool(authKey, true);
+  }
+
+  Future<bool> saveLoginUser(LoginResult loginUser) async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.setString(loginUserKey, loginUser.toJson());
+  }
+
+  Future<bool> deleteLoginUser(LoginResult loginUser) async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.setString(loginUserKey, "");
+  }
+
+  Future<LoginResult?> getLoginUser() async {
+    final preferences = await SharedPreferences.getInstance();
+    final jsonString = preferences.getString(loginUserKey);
+    if (jsonString != null && jsonString.isNotEmpty) {
+      return LoginResult.fromJson(jsonString);
+    }
+    return null;
+  }
 }
