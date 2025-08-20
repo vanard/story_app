85a848cf7ebdda698e6dd174276a059a691f799f
vanard <vrddarknes@yahoo.com>
2025-08-20 16:49:19 +0700

    add dummy add story and detail story

---
+++ lib/screens/add_story_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddStoryScreen extends StatefulWidget {
+  const AddStoryScreen({super.key});
+
+  @override
+  State<AddStoryScreen> createState() => _AddStoryScreenState();
+}
+
+class _AddStoryScreenState extends State<AddStoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/detail_story_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailStoryScreen extends StatelessWidget {
+  const DetailStoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Text('Detail Story Screen'),
+      Text('This is where the story details will be displayed.'),
+    ],);
+  }
+}
