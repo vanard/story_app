702af696239894ad28a0f93560b47644589131f7
vanard <vrddarknes@yahoo.com>
2025-08-21 15:30:13 +0700

    add logic for adding image and upload it, also update ui

--- lib/providers/stories_provider.dart
+++ lib/providers/stories_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:story_app/data/api_service.dart';
 import 'package:story_app/models/list_story.dart';
@@ -8,12 +10,42 @@ class StoriesProvider extends ChangeNotifier {
   bool _isLoading = false;
   String _errorMessage = '';
   List<Story>? _listStory;
+  File? _selectedImage;
 
   StoriesProvider({required ApiService service}) : _apiService = service;
 
   bool get isLoading => _isLoading;
   String get errorMessage => _errorMessage;
   List<Story>? get listStory => _listStory;
+  File? get selectedImage => _selectedImage;
+
+  void setSelectedImage(File? image) {
+    _selectedImage = image;
+    notifyListeners();
+  }
+
+  Future<void> getImageFileSize(String filePath) async {
+    final file = File(filePath);
+    if (await file.exists()) {
+      final int bytes = await file.length();
+
+      final double kb = bytes / 1024;
+      final double mb = kb / 1024;
+
+      debugPrint('File size: $bytes bytes');
+      debugPrint('File size: ${kb.toStringAsFixed(2)} KB');
+      debugPrint('File size: ${mb.toStringAsFixed(2)} MB');
+    } else {
+      debugPrint('File does not exist.');
+    }
+  }
+
+  Story getStoryById(String id) {
+    if (_listStory != null) {
+      return _listStory!.firstWhere((story) => story.id == id);
+    }
+    throw Exception('Story not found');
+  }
 
   Future<void> fetchStories({
     int page = 0,
@@ -28,6 +60,27 @@ class StoriesProvider extends ChangeNotifier {
       final resData = ListStoryResponse.fromMap(stories);
       debugPrint('Fetched stories: ${resData.listStory.length}');
       _listStory = resData.listStory;
+    } catch (e) {
+      _errorMessage = e.toString().split('Exception:').last.trim();
+      debugPrint('Error fetching stories: $_errorMessage');
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> addStory({
+    required String description,
+    required String token,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+    try {
+      await _apiService.addNewStory(
+        _selectedImage!,
+        description,
+        token,
+      );
     } catch (e) {
       _errorMessage = e.toString().split('Exception:').last.trim();
       debugPrint('Error fetching stories: $_errorMessage');
--- lib/screens/add_story_view.dart
+++ lib/screens/add_story_view.dart
@@ -1,4 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:story_app/l10n/app_localizations.dart';
+import 'package:story_app/providers/auth_provider.dart';
+import 'package:story_app/providers/stories_provider.dart';
+import 'package:story_app/routes/router_helper.dart';
 
 class AddStoryScreen extends StatefulWidget {
   const AddStoryScreen({super.key});
@@ -8,8 +17,170 @@ class AddStoryScreen extends StatefulWidget {
 }
 
 class _AddStoryScreenState extends State<AddStoryScreen> {
+  final ImagePicker _imagePicker = ImagePicker();
+  late final _provider = Provider.of<StoriesProvider>(context, listen: false);
+  final _descriptionController = TextEditingController();
+
+  void _openImagePicker() {
+    debugPrint('Open image picker');
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: const Icon(Iconsax.gallery),
+              title: Text(AppLocalizations.of(context)!.gallery),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.gallery);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Iconsax.camera),
+              title: Text(AppLocalizations.of(context)!.camera),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.camera);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _pickImage(ImageSource source) {
+    debugPrint('Pick image from gallery');
+    _imagePicker
+        .pickImage(source: source)
+        .then((image) {
+          if (image != null) {
+            _provider.setSelectedImage(File(image.path));
+            _provider.getImageFileSize(image.path);
+          }
+        })
+        .catchError((error) {
+          debugPrint('Error picking image: $error');
+          if (!mounted) return;
+          _showMessage('Error picking image: $error');
+        });
+  }
+
+  void _submitStory() async {
+    debugPrint('Submit story');
+    final description = _descriptionController.text.trim();
+    if (description.isEmpty) {
+      _showMessage(AppLocalizations.of(context)!.descriptionRequired);
+      return;
+    }
+
+    if (_provider.selectedImage == null) {
+      _showMessage(AppLocalizations.of(context)!.imageRequired);
+      return;
+    }
+
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    final token = authProvider.loginUser?.token ?? '';
+
+    debugPrint('current token: $token');
+
+    try {
+      await _provider.addStory(description: description, token: token);
+      _descriptionController.clear();
+      _provider.setSelectedImage(null);
+      _provider.fetchStories(token: token);
+      if (!mounted) return;
+      _showMessage(AppLocalizations.of(context)!.storyAdded);
+      context.navigateToTab(0);
+    } catch (e) {
+      debugPrint('Error submitting story: $e');
+      _showMessage('Error submitting story: $e');
+    }
+  }
+
+  void _showMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  void dispose() {
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final provider = Provider.of<StoriesProvider>(context);
+    final appLocalizations = AppLocalizations.of(context)!;
+    return Scaffold(
+      appBar: AppBar(title: Text(appLocalizations.addStory)),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            if (provider.selectedImage == null)
+              IconButton(
+                icon: Icon(Iconsax.add),
+                iconSize: 200,
+                onPressed: _openImagePicker,
+              )
+            else
+              GestureDetector(
+                onTap: _openImagePicker,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: Image.file(
+                    _provider.selectedImage ?? File(''),
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+            const SizedBox(height: 30),
+            TextField(
+              controller: _descriptionController,
+              maxLines: 6,
+              decoration: InputDecoration(
+                hintText: appLocalizations.description,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+              style: TextStyle().copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: ElevatedButton(
+                onPressed: _submitStory,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+                child: provider.isLoading
+                    ? const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: CircularProgressIndicator(),
+                      )
+                    : Text(appLocalizations.upload),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
